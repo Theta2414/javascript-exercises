462d31961a315a9642c89105456e1bc2303331a7
Theta <169332632+Theta2414@users.noreply.github.com>
2025-06-13 16:17:41 +0700

    Completed Exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return "ERROR";
+    } else if (start > end) {
+        return (start - end + 1) / 2 * (start + end);
+    } else {
+        return (end - start + 1) / 2 * (start + end);
+    }
 };
 
 // Do not edit below this line
