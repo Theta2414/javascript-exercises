ffc35cccd31ceae9d170ba224df5cba47b02eff4
Theta <169332632+Theta2414@users.noreply.github.com>
2025-06-13 14:56:33 +0700

    Completed exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    numberOfTimes = Math.round(numberOfTimes);
+    if (numberOfTimes < 0) {
+        return "ERROR"
+    } else {
+        let result = "";
+        for (let i = 1; i <= numberOfTimes; i++) {
+            result += string;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
