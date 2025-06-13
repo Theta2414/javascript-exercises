b98f9102dad99f3720fea6bbce36d1fb99eaf398
Theta <169332632+Theta2414@users.noreply.github.com>
2025-06-13 15:57:26 +0700

    Completed Exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...deleteVars) {
+    return arr.filter((item) => !deleteVars.includes(item));
 };
 
 // Do not edit below this line
