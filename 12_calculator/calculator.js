daf5e7efe536186cac28c32768307b528faed658
Theta <169332632+Theta2414@users.noreply.github.com>
2025-08-09 21:44:28 +0700

    Completed excersise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrNum) {
+	return arrNum.reduce((result, num) => result += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrNum) {
+	return arrNum.reduce((result, num) => result *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 2) return 1;
+	return num * factorial(num - 1);
 };
+
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial
 };
