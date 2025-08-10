803388c1157e633a399488616cca33c6c956f882
Theta <169332632+Theta2414@users.noreply.github.com>
2025-08-10 13:15:02 +0700

    Completed excercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((acc, book) => {
+        acc.push(book.title);
+        return acc;
+    }, []);
 };
 
 // Do not edit below this line
