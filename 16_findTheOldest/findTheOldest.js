13aecbe523fe8e5e4904ac84f5544401143b2aa7
Theta <169332632+Theta2414@users.noreply.github.com>
2025-08-10 14:10:23 +0700

    Completed Excercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let ageArr = [];
+    for (person in arr) {
+        if (!("yearOfDeath" in arr[person])) {
+            arr[person].yearOfDeath = new Date().getFullYear();
+        }
+        arr[person].age = arr[person].yearOfDeath - arr[person].yearOfBirth;
+        ageArr.push(arr[person].age);
+    }
+    ageArr.sort((a, b) => b - a);
+    let oldest = ageArr[0];
+    return arr.filter((person) => person.age === oldest)[0];
 };
 
 // Do not edit below this line
