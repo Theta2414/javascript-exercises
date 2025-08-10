3593493ef841d574ac471cf26e0f7393ceae5957
Theta <169332632+Theta2414@users.noreply.github.com>
2025-08-10 12:58:28 +0700

    Completed excercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+let fib = [0, 1, 1];
 
+const fibonacci = function(pos) {
+    pos = Math.floor(Number(pos));
+    if (pos < 0) {
+        return "OOPS";
+    } else if (fib[pos] === undefined) {
+        fib[pos] = fibonacci(pos - 1) + fibonacci(pos - 2);
+    }
+    return fib[pos];
 };
 
 // Do not edit below this line
