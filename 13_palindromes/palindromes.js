6910e498599ac66e8ddbd244832705f04daf541a
Theta <169332632+Theta2414@users.noreply.github.com>
2025-08-10 12:19:03 +0700

    Minor changes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,4 @@
 const palindromes = function (str) {
-    //option 2
     const letters = {
         a: "text",
         b: "text",
@@ -37,20 +36,7 @@ const palindromes = function (str) {
         8: "text",
         9: "text"
     };
-    
-    //Option 1
-    // state = true;
-    // let strArr1 = str.split(" ").join("").split(",").join("").split(".").join("").split("!").join("").split("").map(item => item.toLowerCase());
-    // for (let i = 0; i < strArr1.length; i++) {
-    //     if (strArr1[i] === strArr1[strArr1.length - i - 1]) {
-    //         state = true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-    // return state;
 
-    //option 2
     state = true;
     let newStr = "";
     //Check for all the characters in the string, add to new string if it is text
